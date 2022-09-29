3f3f80ae4fa123c3622833e465a68ca8d8aa5d2f
Adnan Mohamed Z <adnan4d@gmail.com>
2022-09-30 04:18:02 +0530

    Added Checkout Page

--- public/js/add.js
+++ public/js/add.js
@@ -1,25 +1,29 @@
-
 let items = [];
 let item;
 let total = 0;
 let index;
 $(".cart-form").hide()
-function add(id) {
-
-    fetch("/add?id=" + id).then((res) => { return res.json() })
+function add(id, type = "burger") {
+    let url = "/getBurgerData?id=" + id;
+    if (type !== "burger") {
+        url = "/getIngredientData?id=" + id + "&&type=" + type;
+    }
+    console.log(url);
+    fetch(url).then((res) => { return res.json() })
         .then((details) => {
             if (items.length === 0) {
                 $(".cart-form").show()
             }
             item = details;
-            total = parseFloat((total + item.price).toFixed(2));
             let itemExists = items.some((k, i) => {
                 index = i;
                 return (k.id === item.id);
 
             })
+            total = parseFloat((total + item.price).toFixed(2));
 
             if (!itemExists) {
+                details.quantity = 1;
                 items.push(details)
                 $(".cart-items").append(
                     `
@@ -32,10 +36,10 @@ function add(id) {
                                 </div>
                                 <div class="d-flex justify-content-around">
                                     <div class="control-quantity">
-                                        <button class="btn ms-1 p-0" onclick="add('${id}')">
-                                            <i class="fa-solid fa-square-plus"></i>
+                                        <button class="btn p-0" onclick="add('${id}', '${type}')">
+                                            <i class="fa-solid fa-square-plus "></i>
                                         </button>
-                                        <input type="text" class="quantity form-check-input w-25 h-75 text-center" value="1" readonly />
+                                        <input type="text" class="quantity form-check-input m-0 w-25 h-100 text-center" value="1" readonly />
                                         <button class="btn p-0" onclick="remove('${id}')">
                                             <i class="fa-solid fa-square-minus"></i>
                                         </button>
@@ -45,15 +49,18 @@ function add(id) {
                                 <hr>                  
                             </div>           
                             `);
-                
-                
+
+
             }
             else {
+                total = parseFloat((total + item.price).toFixed(2));
+
+                items[index].quantity++;
                 $(".quantity")[index].value = parseInt($(".quantity")[index].value) + 1;
             }
             $("#subtotal").html("₹" + total);
         }).catch((er) => { console.log(er) });
-        // console.log(items);
+    // console.log(items);
 
 }
 
@@ -67,16 +74,27 @@ function remove(id) {
     console.log(index);
     total = parseFloat((total - items[index].price).toFixed(2));
 
-    $("#subtotal").html("₹" +total);
+    $("#subtotal").html("₹" + total);
     if (parseInt($(".quantity")[index].value) - 1 === 0) {
         $(".item")[index].remove();
-        items.splice(index,1);
+        items.splice(index, 1);
 
-        if(items.length === 0){
+        if (items.length === 0) {
             $(".cart-form").hide();
         }
     } else {
-
+        items[index].quantity++;
         $(".quantity")[index].value = parseInt($(".quantity")[index].value) - 1;
     }
 }
+
+function checkout(){
+    items = items.map(i=>JSON.stringify(i));
+    console.log(items);
+    sessionStorage.setItem("cart", items);
+    window.location.assign("checkout");
+}
+
+
+
+
